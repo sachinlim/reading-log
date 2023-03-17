2b3d09a961512afa75d4c1fd5511eba5b92f1658
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-03-17 16:36:00 +0000

    changing requirements for username

--- readings/models.py
+++ readings/models.py
@@ -4,14 +4,13 @@ from django.contrib.auth.models import User
 
 class Reading(models.Model):
     # if a user is deleted, their logs are deleted
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
     pages = models.CharField(max_length=5)
-    current_page = models.CharField(max_length=5, null=True, blank=True)
-
     started_reading = models.DateField(null=True, blank=True)
+    current_page = models.CharField(max_length=5, null=True, blank=True)
     finished_reading = models.DateField(null=True, blank=True)
 
     thoughts = models.TextField(null=True, blank=True)
