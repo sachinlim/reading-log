f7ce8c0cc9c4a615f6b0c2abaa005b909b075e0d
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-03-17 16:34:20 +0000

    adding login and register classes

--- readings/views.py
+++ readings/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from django.urls import reverse_lazy
 from .models import Reading
 
 
-class ReadingList(ListView):
-    # looks for reading_list.html
+class ReadingList(LoginRequiredMixin, ListView):
+    """
+    Displays all the books that the user has stored on the database
+    The template separates currently reading books from books that have not been started
+    Looks for reading_list.html by default
+    """
     model = Reading
     context_object_name = 'reading_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reading_list'] = context['reading_list'].filter(user=self.request.user)
+        return context
 
-class Book(DetailView):
-    # looks for reading_detail.html
+
+class Book(LoginRequiredMixin, DetailView):
+    """
+    Displays information about books in a separate page
+    Passes the ID to be able to view them
+    Looks for reading_detail.html by default
+    """
     model = Reading
     context_object_name = 'book'
 
@@ -21,24 +39,74 @@ class Book(DetailView):
     template_name = 'readings/book.html'
 
 
-class CreateBook(CreateView):
-    # looks for reading_form.html
-    # will use the models that have been created, and makes a form based on those models and their fields
+class CreateBook(LoginRequiredMixin, CreateView):
+    """
+    Generates a form where the user can add new books onto the database - only asks for a few user inputs
+    Will use the models that has been created, and makes a form based on those models and their fields
+    Error messages shown by Django's built-in methods
+    Looks for reading_form.html by default
+    """
     model = Reading
-    fields = '__all__'
+    fields = ['title', 'author', 'genre', 'pages', 'started_reading', 'current_page']
+    success_url = reverse_lazy('reading-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateBook, self).form_valid(form)
+
+
+class UpdateBook(LoginRequiredMixin, UpdateView):
+    """
+    Displays a form filled with data for the selected book - all the fields are shown except username
+    Will use the model that has been created, and makes a form based on those models and their fields
+    Error messages shown by Django's built-in methods
+    Looks for reading_form.html by default
+    """
+    model = Reading
+    fields = ['title', 'author', 'genre', 'pages', 'started_reading', 'current_page', 'finished_reading',
+              'thoughts', 'summary', 'recommended']
     success_url = reverse_lazy('reading-list')
 
 
-class UpdateBook(UpdateView):
-    # looks for reading_form.html
-    model = Reading
-    fields = '__all__'
-    success_url = reverse_lazy('reading-list')
-
-
-class DeleteBook(DeleteView):
-    # looks for reading_confirm_delete.html
+class DeleteBook(LoginRequiredMixin, DeleteView):
+    """
+    Deletes the selected book - asks to confirm before deleting
+    Looks for reading_confirm_delete.html by default
+    """
     model = Reading
     context_object_name = 'book'
     success_url = reverse_lazy('reading-list')
 
+
+class UserLogin(LoginView):
+    """
+    User login form - redirects to the readings-list view once successful
+    Error messages shown by Django's built-in methods
+    Logging out is done directly from views.py
+    """
+    template_name = 'readings/login.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('reading-list')
+
+
+class RegisterUser(FormView):
+    """
+    Allows for new users to sign up
+    Shows default Django requirements for username and password
+    """
+    template_name = 'readings/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('reading-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterUser, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('reading-list')
+        return super(RegisterUser, self).get(*args, **kwargs)
