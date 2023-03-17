5bb3285f091ab011f5fef03bdb0a28b87e4de4d6
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-03-17 16:35:12 +0000

    adding login, logout and register views

--- readings/urls.py
+++ readings/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import ReadingList, Book, CreateBook, UpdateBook, DeleteBook
+from .views import ReadingList, Book, CreateBook, UpdateBook, DeleteBook, UserLogin, RegisterUser
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    # authentication
+    path('login/', UserLogin.as_view(), name='user-login'),
+    path('logout/', LogoutView.as_view(next_page='user-login'), name='logout'),
+    path('register/', RegisterUser.as_view(), name='register-user'),
+
+    # webs navigation and CRUD
     path('', ReadingList.as_view(), name='reading-list'),
     path('book/<int:pk>', Book.as_view(), name='book'),
     path('create-book/', CreateBook.as_view(), name='create-book'),
