aa1b9d2c49a188ae996f6d820386a57f152cff74
Sachin Limbu <80691974+sachinlim@users.noreply.github.com>
2023-03-18 18:16:57 +0000

    Adding login page for redirecting access when users aren't logged in

--- reading_log/settings.py
+++ reading_log/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'readings.apps.ReadingsConfig'
+    'readings.apps.ReadingsConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LOGIN_URL = 'user-login'
 
 
 # Static files (CSS, JavaScript, Images)
